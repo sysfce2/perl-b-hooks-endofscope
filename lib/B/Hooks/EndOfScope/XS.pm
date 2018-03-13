044195afe9c9490e855fcc04fa657cf8d3268f86
Peter Rabbitson <ribasushi@leporine.io>
2018-03-13 18:16:42 +0100

    Fix memory corruption on perls 5.8.0 ~ 5.8.3

--- lib/B/Hooks/EndOfScope/XS.pm
+++ lib/B/Hooks/EndOfScope/XS.pm
@@ -6,6 +6,15 @@ use warnings;
 
 our $VERSION = '0.22';
 
+# Limit the V::M-based (XS) version to perl 5.8.4+
+#
+# Given the unorthodox stuff we do to work around the hinthash double-free
+# might as well play it safe and only implement it in the PP version
+# and leave it at that
+# https://rt.perl.org/Public/Bug/Display.html?id=27040#txn-82797
+#
+use 5.008004;
+
 use Variable::Magic 0.48 ();
 use Sub::Exporter::Progressive 0.001006 -setup => {
   exports => ['on_scope_end'],
